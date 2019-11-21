78c111277c828f941ec2e7c9744bbed61c312a32
Cat Templeton <10134142+idivait@users.noreply.github.com>
2019-11-21 13:45:41 -0600

    Add in discord modules

--- express/server.js
+++ express/server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const moment = require('moment');
+const discord = require('./routes/discord');
 
+const app = express();
 const router = express.Router();
+
+// Base Routing
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
@@ -16,97 +17,15 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-let status = {
-  "status" : {
-    "code" : "200",
-    "msg" : "Message sent to Discord!"
-  }
-};
-
-router.post('/discord', (req, res)=> {
-
-  // Get the data
-  const permalink = req.body.permalink ? req.body.permalink : req.query.permalink;
-  const DR_JSON_EP = `https://dps.report/getJson?permalink=${permalink}`;
-  const DISCORD_EP = `https://discordapp.com/api/webhooks/646892584454848544/4QqmHrt-krxAR0ehONPF8yuItj_jVadNjYMkywOdQxtQms_hCXKwfgEQjsBC56cLsQ5h`;
-
-  axios.post(DR_JSON_EP).then(response => {
-    let { recordedBy, timeStart, duration, players } = response.data;
-    let squadCount = players.length;
-    let groupCount = players.reduce(function(total, currentValue, currentIndex, arr){
-      if ( !total[currentValue.group] ) {
-        total[currentValue.group] = 1;
-      } else {
-        total[currentValue.group]++;
-      }
-      return total;
-    }, {});
-
-    let start = moment(timeStart, "YYYY-MM-DD HH:mm:ss Z");
-    
-    let embed = {
-      "content" : "",
-      "embeds" : [
-        {
-          "color" : 2451166,
-          "title" : `New Log`,
-          "author" : {
-            "name" : recordedBy
-          },
-          "description" : "",
-          "fields" : [
-            {
-              "name" : "URL",
-              "value" : permalink
-            },
-            {
-              "name" : "Started",
-              "value" : start.format("ddd, h:mmA"),
-              "inline" : true
-            },
-            {
-              "name" : "Duration",
-              "value" : duration,
-              "inline" : true
-            },
-            {
-              "name" : "Subgroups",
-              "value" : Object.keys(groupCount).length,
-              "inline" : true
-            },
-            {
-              "name" : "Squad Count",
-              "value" : squadCount,
-              "inline" : true
-            }
-          ],
-          "timestamp" : new Date(),
-        }
-      ]
-    };
-
-    axios.post( DISCORD_EP, embed )
-      .then(response=>{
-        console.log(response)
-      })
-      .catch(error=>{
-        console.error(error);
-      });
-  });
-
-  res.json(status);
-
-});
-
-router.get('/discord/pingtest', (req, res)=> {
-  res.json(status);
-});
-
+// Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 })); 
+
+// Routes setup
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server/discord', discord);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
